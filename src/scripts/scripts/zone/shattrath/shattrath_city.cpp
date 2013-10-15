806a0a1789389f1a15ee2ca8f2da6c4c88c05b59
Xadras <support@looking4group.de>
2013-10-15 17:18:49 +0200

    Script/DB: Add a Rent a Mount System :D (ty ysfl)

--- src/scripts/scripts/zone/shattrath/shattrath_city.cpp
+++ src/scripts/scripts/zone/shattrath/shattrath_city.cpp
@@ -1008,6 +1008,157 @@ bool ChooseReward_npc_Adal(Player *player, Creature *creature, const Quest *ques
 }
 
 
+#define MSG_GOSSIP_TEXT_1  "I would like to rent a Ancient Frostsaber" 
+#define MSG_GOSSIP_TEXT_2  "I would like to rent a Swift Brewfest Ram" 
+#define MSG_GOSSIP_TEXT_3  "I would like to rent a Black War Kodo"
+#define MSG_GOSSIP_TEXT_4  "I would like to rent a Swift Blue Raptor"
+#define MSG_GOSSIP_TEXT_5  "I would like to rent a Black War Tiger"
+#define MSG_GOSSIP_TEXT_6  "I would like to rent a Amani War Bear"
+#define MSG_GOSSIP_TEXT_7  "I would like to rent a Mechanostrider"
+#define MSG_GOSSIP_TEXT_8  "I would like to rent a Raven Lord"
+#define MSG_GOSSIP_TEXT_9  "I would like to rent a black Gryphon"
+#define MSG_GOSSIP_TEXT_10 "I would like to rent Alar"
+#define MSG_NOT_MONEY      "You do not have enough money." 
+#define MSG_MOUTED         "You already have a mount." 
+#define MOUNT_SPELL_ID_1   16056 
+#define MOUNT_SPELL_ID_2   43900 
+#define MOUNT_SPELL_ID_3   22718 
+#define MOUNT_SPELL_ID_4   23241 
+#define MOUNT_SPELL_ID_5   22723 
+#define MOUNT_SPELL_ID_6   43688 
+#define MOUNT_SPELL_ID_7   17453 
+#define MOUNT_SPELL_ID_8   41252
+#define MOUNT_SPELL_ID_9   32239
+#define MOUNT_SPELL_ID_10  40192
+#define NEW_GOSSIP_MESSAGE 40000
+bool GossipHello_npc_rentalmount(Player *player, Creature *_creature) 
+{ 
+    player->ADD_GOSSIP_ITEM(4, MSG_GOSSIP_TEXT_1, GOSSIP_SENDER_MAIN, 1001); 
+    player->ADD_GOSSIP_ITEM(4, MSG_GOSSIP_TEXT_2, GOSSIP_SENDER_MAIN, 1002); 
+    player->ADD_GOSSIP_ITEM(4, MSG_GOSSIP_TEXT_3, GOSSIP_SENDER_MAIN, 1003); 
+    player->ADD_GOSSIP_ITEM(4, MSG_GOSSIP_TEXT_4, GOSSIP_SENDER_MAIN, 1004); 
+    player->ADD_GOSSIP_ITEM(4, MSG_GOSSIP_TEXT_5, GOSSIP_SENDER_MAIN, 1005); 
+    player->ADD_GOSSIP_ITEM(4, MSG_GOSSIP_TEXT_6, GOSSIP_SENDER_MAIN, 1006); 
+    player->ADD_GOSSIP_ITEM(4, MSG_GOSSIP_TEXT_7, GOSSIP_SENDER_MAIN, 1007); 
+    player->ADD_GOSSIP_ITEM(4, MSG_GOSSIP_TEXT_8, GOSSIP_SENDER_MAIN, 1008); 
+    player->ADD_GOSSIP_ITEM(4, MSG_GOSSIP_TEXT_9, GOSSIP_SENDER_MAIN, 1009); 
+    player->ADD_GOSSIP_ITEM(4, MSG_GOSSIP_TEXT_10, GOSSIP_SENDER_MAIN, 1010); 
+    player->SEND_GOSSIP_MENU(NEW_GOSSIP_MESSAGE, _creature->GetObjectGuid()); 
+    return true; 
+} 
+
+bool GossipSelect_npc_rentalmount(Player *player, Creature *_creature, uint32 sender, uint32 action ) 
+{ 
+    if (sender != GOSSIP_SENDER_MAIN) 
+        return false; 
+
+    if (player->IsMounted()){ 
+        _creature->MonsterSay(MSG_MOUTED,NULL,player->GetGUID()); 
+         return false; 
+    } 
+
+    int32 fastprice = 50000;
+    int32 highprice = 250000;
+    int32 ultraprice = 500000;
+
+    switch (action) 
+    { 
+    case 1001: 
+        if (player->GetMoney() < fastprice) 
+        { 
+            _creature->MonsterSay(MSG_NOT_MONEY, NULL,player->GetGUID());
+        } else { 
+            player->CastSpell(player,MOUNT_SPELL_ID_1,false);
+            player->ModifyMoney(-fastprice); 
+        } 
+        break; 
+    case 1002: 
+        if (player->GetMoney() < fastprice) 
+        { 
+            _creature->MonsterSay(MSG_NOT_MONEY, NULL,player->GetGUID());
+        } else { 
+            player->CastSpell(player,MOUNT_SPELL_ID_2,false); 
+            player->ModifyMoney(-fastprice); 
+        } 
+        break; 
+    case 1003: 
+        if (player->GetMoney() < fastprice) 
+        { 
+            _creature->MonsterSay(MSG_NOT_MONEY, NULL,player->GetGUID()); 
+        } else { 
+            player->CastSpell(player,MOUNT_SPELL_ID_3,false); 
+            player->ModifyMoney(-fastprice); 
+        } 
+        break; 
+    case 1004: 
+        if (player->GetMoney() < fastprice) 
+        { 
+            _creature->MonsterSay(MSG_NOT_MONEY, NULL,player->GetGUID());
+        } else { 
+            player->CastSpell(player,MOUNT_SPELL_ID_4,false); 
+            player->ModifyMoney(-fastprice); 
+        } 
+        break; 
+   case 1005: 
+        if (player->GetMoney() < fastprice) 
+        { 
+            _creature->MonsterSay(MSG_NOT_MONEY, NULL,player->GetGUID());
+        } else { 
+            player->CastSpell(player,MOUNT_SPELL_ID_5,false); 
+            player->ModifyMoney(-fastprice); 
+        } 
+        break; 
+    case 1006: 
+        if (player->GetMoney() < fastprice) 
+        { 
+            _creature->MonsterSay(MSG_NOT_MONEY, NULL,player->GetGUID()); 
+        } else { 
+            player->CastSpell(player,MOUNT_SPELL_ID_6,false); 
+            player->ModifyMoney(-fastprice); 
+        } 
+        break; 
+    case 1007: 
+        if (player->GetMoney() < fastprice) 
+        { 
+            _creature->MonsterSay(MSG_NOT_MONEY, NULL,player->GetGUID());
+        } else { 
+            player->CastSpell(player,MOUNT_SPELL_ID_7,false); 
+            player->ModifyMoney(-fastprice); 
+        } 
+        break; 
+    case 1008: 
+        if (player->GetMoney() < fastprice) 
+        { 
+            _creature->MonsterSay(MSG_NOT_MONEY, NULL,player->GetGUID());
+        } else { 
+            player->CastSpell(player,MOUNT_SPELL_ID_8,false); 
+            player->ModifyMoney(-fastprice); 
+        } 
+        break; 
+    case 1009: 
+        if (player->GetMoney() < highprice) 
+        { 
+            _creature->MonsterSay(MSG_NOT_MONEY, NULL,player->GetGUID());
+        } else { 
+            player->CastSpell(player,MOUNT_SPELL_ID_9,false); 
+            player->ModifyMoney(-highprice); 
+        }
+    case 1010: 
+        if (player->GetMoney() < ultraprice) 
+        { 
+            _creature->MonsterSay(MSG_NOT_MONEY, NULL,player->GetGUID());
+        } else { 
+            player->CastSpell(player,MOUNT_SPELL_ID_10,false); 
+            player->ModifyMoney(-ultraprice); 
+        } 
+        break; 
+        break; 
+
+    } 
+    player->CLOSE_GOSSIP_MENU();
+    return true; 
+} 
+
 void AddSC_shattrath_city()
 {
     Script *newscript;
@@ -1069,5 +1220,11 @@ void AddSC_shattrath_city()
     newscript->Name="npc_adal";
     newscript->pQuestRewardedNPC = &ChooseReward_npc_Adal;
     newscript->RegisterSelf();
+
+     newscript = new Script; 
+     newscript->Name = "npc_rentalmount"; 
+     newscript->pGossipHello = &GossipHello_npc_rentalmount; 
+     newscript->pGossipSelect = &GossipSelect_npc_rentalmount; 
+     newscript->RegisterSelf(); 
 }
 
