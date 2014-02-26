540ad256cd2ec009624e4beb3bcedcc6e1de37cd
Xadras <support@looking4group.de>
2014-02-26 11:10:09 +0100

    Script/Quest: Akama - stun remove after event

--- src/scripts/scripts/zone/shadowmoon_valley/shadowmoon_valley.cpp
+++ src/scripts/scripts/zone/shadowmoon_valley/shadowmoon_valley.cpp
@@ -2072,7 +2072,7 @@ struct npc_AkamaAI : public ScriptedAI
                         target->SetFlag(UNIT_FIELD_FLAGS_2, UNIT_FLAG2_FEIGN_DEATH);
                         target->SetHealth(1);
                         ((Player*)target)->setRegenTimer(60000);
-                        target->GetUnitStateMgr().PushAction(UNIT_ACTION_STUN);
+                        target->GetUnitStateMgr().DropAction(UNIT_ACTION_STUN);
                     }
                 }
                 return 3000;
