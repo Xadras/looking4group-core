2b5698d25d965902048d3dffc4dd67536fda1a30
Xadras <support@looking4group.de>
2014-06-03 13:58:10 +0200

    Script/Alar: Fix tempory umlauts in new yell This is just temporary because i must find a function for yellow text in the middle of the screen

--- src/scripts/scripts/zone/tempest_keep/the_eye/boss_alar.cpp
+++ src/scripts/scripts/zone/tempest_keep/the_eye/boss_alar.cpp
@@ -434,7 +434,7 @@ struct boss_alarAI : public ScriptedAI
                     {
                         cur_wp = 4;
                         WaitEvent = WE_QUILL;
-                        m_creature->Yell("Denkt ihr tatsächlich, dass ihr eine Chance habt? Seht selbst...", LANG_UNIVERSAL, me->getVictimGUID());
+                        m_creature->Yell("Denkt ihr tatsaechlich, dass ihr eine Chance habt? Seht selbst...", LANG_UNIVERSAL, me->getVictimGUID());
                     }
                 }
 
